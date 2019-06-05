6c6560e357669de7e472c0b122312927b4838f60
Fernando <fernando_lopezc@live.com>
2019-06-05 11:33:49 +0100

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,32 @@
 // Code ClickityClick Component Here
+import React, {Component} from 'react'
+
+export default class ClickityClick extends Component {
+
+    state = {
+        hasBeenClicked: false
+    }
+
+    clickedToggler = () => {
+    this.setState(prevState => {
+        return {
+            hasBeenClicked: !prevState.hasBeenClicked
+        }   
+    }
+    )}
+
+    // this.setState((prevState, props) => {
+    //     return {counter: prevState.counter + props.step};
+    //   })
+
+    render(){
+        return(
+            <div>
+                <p> I have {this.state.hasBeenClicked? null: "not"} been clicked!</p>
+                <button onClick = {this.clickedToggler.bind(this)}>Click me!</button>
+            </div>
+
+        )
+    }
+
+}
